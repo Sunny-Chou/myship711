81316db5384e922120b000127bc58edd4009be7a
unknown <a0983452080@gmail.com>
2024-06-15 17:52:52 +0800

    1

--- public/transfer.js
+++ public/transfer.js
@@ -3,26 +3,26 @@ function handleMouseEnter(div, sevicer) {
     const p1 = div.querySelector('.avatar-content p');
     const div2 = div.querySelector('.avatar-content div');
     const img = div.querySelector('img');
-    const button1 = div.querySelector('button:first-child');
-    const button2 = div.querySelector('button:last-child');
-    
+    const button1 = div.querySelector('> button:first-child');
+    const button2 = div.querySelector('> button:last-child');
+    button1.classList.remove('hidden');
     if (sevicer !== '') button2.classList.remove('hidden');
     p1.classList.add('hidden');
     div2.classList.add('hidden');
-    img.classList.add('hidden');button1.classList.remove('hidden');
+    img.classList.add('hidden');
 }
 
 function handleMouseLeave(div, sevicer) {
     const p1 = div.querySelector('.avatar-content p');
     const div2 = div.querySelector('.avatar-content div');
     const img = div.querySelector('img');
-    const button1 = div.querySelector('button:first-child');
-    const button2 = div.querySelector('button:last-child');
-    
+    const button1 = div.querySelector('> button:first-child');
+    const button2 = div.querySelector('> button:last-child');
+    button1.classList.add('hidden');
     if (sevicer !== '') button2.classList.add('hidden');
     p1.classList.remove('hidden');
     div2.classList.remove('hidden');
-    img.classList.remove('hidden');button1.classList.add('hidden');
+    img.classList.remove('hidden');
 }
 ws.onopen = function (event) {
     ws.send(JSON.stringify({ type: "getclient", userId: sessionStorage.getItem("userId") }));
